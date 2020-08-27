cbbdc15d11ef7e01d8dd8c013f851f64ca3ddca8
AnithaRaman20 <aniraman@in.ibm.com>
2020-08-27 12:14:37 +0530

    first commit

---
+++ MobileApp.java
@@ -0,0 +1,42 @@
+package Assignment;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class MobileApp {
+	public static void main(String[] args) throws MalformedURLException, InterruptedException{
+		//define capability if this is present/matches my real device capability it should run
+		DesiredCapabilities cap= new DesiredCapabilities ();
+		//common capabilities required to test an app
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Anitha");
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		//add capbility for web app
+		cap.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
+
+		//chrome driver executable specific to android
+		cap.setCapability(AndroidMobileCapabilityType.CHROMEDRIVER_EXECUTABLE, "C:\\Users\\AnithaRaman\\Downloads\\Jars-Selenium\\Mobile\\chromedriver_win32\\chromedriver.exe");
+
+		AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://0.0.0.0:4723/wd/hub"),cap);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://m.cricbuzz.com");
+		WebElement element=driver.findElement(By.xpath("//*[contains(text(),'Menu')]"));
+		element.click();
+		driver.findElement(By.xpath("//a[contains(text(),'Home')]")).click();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//*[contains(text(),'England clinch series 1-0')]")).click();
+				
+		
+		
+	}
+}
